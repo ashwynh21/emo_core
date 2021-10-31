330b88484bd4173d85d8a4d0bf9522909728606b
Ashwyn D. Horton <ashwynh21@gmail.com>
2021-10-31 12:26:55 +0200

    added mail listener a reemition functionality to the application definition

---
+++ declarations/application.ts
@@ -0,0 +1,136 @@
+/*
+ * application -
+ * here we are going to define a base class that will extend the imap Connection definition so that we can configure
+ * custom features to the client
+ * */
+import Connection, { Box, parseHeader } from 'imap';
+import { EventEmitter } from 'events';
+// we import reflect-meta for some decorators
+import 'reflect-metadata';
+
+interface Options {
+    user: string;
+    password: string;
+    host: string;
+    port: number;
+}
+export class Application extends EventEmitter {
+    private context: Connection;
+    inbox: Box | undefined;
+
+    constructor(options: Options) {
+        super();
+
+        this.context = new Connection({
+            ...options,
+            tls: true,
+            // we add this key so we do not get the self signed certificate error
+            tlsOptions: { rejectUnauthorized: false },
+        });
+
+        // once constructed we will bind in the decorated functions for the event listeners
+        this.bind();
+    }
+    // We define a function that will bind functions of a class as a listener to an instance of a client reference
+    private bind() {
+        // we begin by getting any meta data tagged to this class instance as its target
+        Reflect.getMetadataKeys(this).forEach((a) => {
+            const { propertyKey, eventName } = Reflect.getMetadata(a, this) as {
+                propertyKey: keyof Application;
+                eventName: string;
+            };
+
+            // so we will check the typeof property to make sure it is a function then we will pass as the listener
+            // callback to the corresponding eventName
+            if (typeof this[propertyKey] == 'function') {
+                this.context.on(eventName, (...args: any[]) =>
+                    (this[propertyKey] as (...args: any[]) => void)(...args),
+                );
+            }
+        });
+    }
+
+    // we define a function called listen which is very akin to server structures and it will run a couple of things
+    listen() {
+        this.context.connect();
+    }
+
+    // we define a function that will handle a ready event
+    @listen()
+    private ready() {
+        // so we should open the inbox so that we are able to go straight to the messages
+        this.context.openBox('INBOX', true, (error, mailbox) => {
+            if (error) this.emit('error', error);
+
+            this.inbox = mailbox;
+            // we will emit the ready event with a mailbox instance so that a user is able to readily check the
+            // mailbox structure
+            this.emit('ready', mailbox);
+        });
+    }
+
+    // we add a function that will listen to or for new emails
+    @listen()
+    private mail() {
+        // so we want this mail event to emit a parsed email object right
+        const query = this.inbox?.messages.total ?? 0;
+
+        const result = this.context.seq.fetch(`${query}:*`, {
+            bodies: ['1', '1.MIME', '2', '2.MIME', '', 'HEADER'],
+            struct: true,
+        });
+
+        // we define a cache instance
+        const cache: any = {};
+        // then we get each message
+        result.on('message', (message, seqno) => {
+            const email: Record<string, string> = {};
+
+            // then we listen for the message events
+            message.on('body', async (stream: any, information: { which: string }) => {
+                // then we write from the stream by combining the streams through the parser instance
+                email[information.which] = await new Promise<string>((resolve) => {
+                    const buffer: Uint8Array[] = [];
+                    stream.on('data', (chunk: Uint8Array) => buffer.push(chunk));
+                    stream.on('end', () => resolve(Buffer.concat(buffer).toString()));
+                });
+            });
+            message.on('end', async () => {
+                // then we go through each stream and write to the parser
+                cache[seqno.toString()] = {
+                    HEADER: await parseHeader(email['HEADER']),
+                    '1': email['1'].split('\r\n').join(''),
+                    '2': email['2'],
+                    '1.MIME': await parseHeader(email['1.MIME']),
+                    '2.MIME': await parseHeader(email['2.MIME']),
+                };
+            });
+        });
+        result.on('end', () => {
+            // so when the query result is done we should have a complete set of messages or mails that we can emit out
+            this.emit('mail', cache);
+        });
+        result.on('error', (error) => this.emit('error', error));
+    }
+}
+
+/**
+ * A decorator function that will bind to the event listener of a client. the name of the event can be passed in
+ * in case the function name does not match the desired event name
+ * @param event string
+ * */
+export function listen(event?: string) {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        Reflect.defineMetadata(
+            propertyKey,
+            {
+                // we spread any meta data that was already present,
+                ...Reflect.getMetadata(propertyKey, target),
+                propertyKey,
+                eventName: event ?? propertyKey,
+            },
+            target,
+        );
+        return descriptor;
+    };
+}
---
+++ lib/index.ts
@@ -0,0 +1,9 @@
+import { Application } from '../declarations/application';
+
+const app = new Application({
+    user: 'ashwynh21@gmail.com',
+    password: 'as28hw01yn95ho61rt00on',
+    host: 'imap.gmail.com',
+    port: 993,
+});
+app.listen();
